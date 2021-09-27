45dd84f377c36574e5969d65249086dba121280c
mgoerens <mgoerens@redhat.com>
2021-09-27 11:54:49 +0200

    Remove unecessary export

--- create_repo.sh
+++ create_repo.sh
@@ -98,9 +98,9 @@ if $INSTALL_OPERATOR_SDK; then
   echo "----Version: $OPERATOR_SDK_VERSION"
 
   # Download binary
-  export ARCH=$(case $(uname -m) in x86_64) echo -n amd64 ;; aarch64) echo -n arm64 ;; *) echo -n $(uname -m) ;; esac)
-  export OS=$(uname | awk '{print tolower($0)}')
-  export OPERATOR_SDK_DL_URL=https://github.com/operator-framework/operator-sdk/releases/download/${OPERATOR_SDK_VERSION}
+  ARCH=$(case $(uname -m) in x86_64) echo -n amd64 ;; aarch64) echo -n arm64 ;; *) echo -n $(uname -m) ;; esac)
+  OS=$(uname | awk '{print tolower($0)}')
+  OPERATOR_SDK_DL_URL=https://github.com/operator-framework/operator-sdk/releases/download/${OPERATOR_SDK_VERSION}
   curl -LO "${OPERATOR_SDK_DL_URL}"/operator-sdk_"${OS}"_"${ARCH}"
   gpg --keyserver keyserver.ubuntu.com --recv-keys 052996E2A20B5C7E
   curl -LO "${OPERATOR_SDK_DL_URL}"/checksums.txt
