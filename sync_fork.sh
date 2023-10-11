40d8bba5bdbee3948ab668278e4ece2e8282add1
mgoerens <mgoerens@redhat.com>
2023-10-11 11:03:24 +0200

    stash / unstash changes

--- sync_fork.sh
+++ sync_fork.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
 
-echo "Syncing fork with upstream"
+echo "> Syncing fork with upstream"
 
 ## Get default branch
 DEFAULT_BRANCH=$(git remote show upstream | awk '/HEAD branch/ {print $NF}')
-echo "Default branch: $DEFAULT_BRANCH"
+echo "> Default branch: $DEFAULT_BRANCH"
 
-echo "Checkout default branch"
+echo "> Stash changes if needed"
+stash=0
+git status --porcelain | grep "^." >/dev/null
+if [ $? -eq 0 ]; then
+  git stash
+  stash=1
+fi
+
+echo "> Checkout default branch"
 git switch $DEFAULT_BRANCH
 
 git fetch --all
 
-echo "Rebase default branch on upstream"
+echo "> Rebase default branch on upstream"
 git rebase upstream/$DEFAULT_BRANCH
 
-echo "Printing status"
+echo "> Printing status"
 git status
 
-echo "Push default branch to origin"
+echo "> Push default branch to origin"
 git push
 
-echo "Checkout previous	branch"
+echo "> Checkout previous branch"
 git switch -
 
-echo "Done syncing fork"
+echo "> Pop stashed changes, if needed"
+if [ $stash -eq 1 ]; then
+  git stash pop
+fi
+
+echo "> Done syncing fork"
 
 
 #echo $PWD
